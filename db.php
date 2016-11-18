13797bc84bce84ddaf0d70c7aee04744c93a3d74
425cedar <sdemerpublic@gmail.com>
2016-11-18 12:24:02 -0500

    Update db.php

--- db.php
+++ db.php
@@ -2,21 +2,25 @@
 $servername = "mysql-instance.clfjswptmpv4.us-west-2.rds.amazonaws.com";
 $username = "read-only";
 $password = "Pa55word";
+$dbname = "mysql_php";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
-
+$conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
-echo "Connected successfully";
 
+$sql = "SELECT id, first, last, sex FROM users";
+$result = $conn->query($sql);
 
-$result = mysql_query("SELECT * FROM users");
-if (!$result) {
-    echo 'Could not run query: ' . mysql_error();
-    exit;
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["id"]. " - Name: " . $row["first"]. " " . $row["last"]. " " . $row["sex"]. "<br>";
+    }
+} else {
+    echo "0 results";
 }
-echo $result;
+$conn->close();
 ?>
